7083a08407eddaaf3c1564b730d995cab6da84a1
Moritz Bugiel <mc.bugiel@googlemail.com>
2020-11-16 20:46:39 +0100

    router for offices

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :offices
+end
+
+
+
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create, :new]
+  end
+  resources :doses, only: [:destroy]
 end

79f49eff63d3e896699df4ad1fa3a66d7cfeb0e0
Moritz Bugiel <mc.bugiel@googlemail.com>
2020-11-16 21:09:04 +0100

    offices controller with all actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ end
 
 
 
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'cocktails#index'
-  resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:create, :new]
-  end
-  resources :doses, only: [:destroy]
-end
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   root to: 'cocktails#index'
+#   resources :cocktails, only: [:index, :show, :new, :create] do
+#     resources :doses, only: [:create, :new]
+#   end
+#   resources :doses, only: [:destroy]
+# end

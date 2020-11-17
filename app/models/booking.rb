e511de68cff45c589356e08ffa83e3087fe82a7d
Jessica Lacroix <jess.dengler@gmail.com>
2020-11-17 13:51:34 +0100

    bookings model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,7 @@
 class Booking < ApplicationRecord
+  belongs_to :office
+  belongs_to :user
+
+  validates :booking_start, presence: true
+  validates :booking_end, presence: true
 end
--- db/migrate/20201117102548_create_bookings.rb
+++ db/migrate/20201117102548_create_bookings.rb
@@ -3,8 +3,8 @@ class CreateBookings < ActiveRecord::Migration[6.0]
     create_table :bookings do |t|
       t.references :office, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      t.datetime booking_start
-      t.datetime booking_end
+      t.datetime :booking_start
+      t.datetime :booking_end
      
       t.timestamps
     end
